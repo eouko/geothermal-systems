1293ffde238e09cd0189535c0bccd23c84798487
Edwin O. Ouko <83521363+eouko@users.noreply.github.com>
2025-05-09 14:36:27 -0400

    Create utils.jl

---
+++ optimization_algorithms/utils.jl
@@ -0,0 +1,14 @@
+# Auxiliary functions  ##########################################################
+function save(path,name,var,rx,ry,rz,t)
+    vtk_grid("$path/$name-$t", rx, ry, rz; compress = false, append = false, ascii = false) do vtk
+        vtk[name] = var
+    end
+end
+
+function plot_ϕ(ϕa,ϕc,ϕbw,rzb,t)
+    plot(rzb, ϕa[1,:].-273.15, label="Temperature at DBHE annulus fluid [°C].")
+    plot!(rzb, ϕc[1,:].-273.15, label="Temperature at DBHE center fluid [°C].")
+    # plot!(rzb, ϕbw[1,:].-273.15, label="Temperature at DBHE wall [°C].") #initially commented
+    plot!(xlabel="depth [m]", ylabel="Temperature [°C]", ylims=(5, 40))
+    savefig("$path/temp-dbhe-$t.png")
+end
